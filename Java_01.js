56d57a752504ac1593599d485151567e17920802
RodionovaMA <113350635+RodionovaMA@users.noreply.github.com>
2023-02-27 09:33:23 +0300

    Add files via upload

---
+++ Java_01.js
@@ -0,0 +1,23 @@
+"Привет, Java!"
+var myThing;
+myThing=5;
+myThing="это строка";
+var numberNine = 9;
+var stringNine = "9";
+numberNine + numberNine;
+stringNine + stringNine;
+var greeting = "Привет,";
+var myName = "Ник";
+greeting + myName;
+myName.length;
+
+var name_orz="ОРЗ", name_bronhit="Бронхит";
+num_orz_mon=4;
+num_bronhit_mon=6;
+num_orz_tues=2;
+num_bronhit_tues=3;
+num_orz_wedn=1;
+num_bronhit_wedn=1;
+num_orz_week=num_orz_mon + num_orz_tues + num_orz_wedn;
+num_bronhit_week=num_bronhit_mon + num_bronhit_tues + num_bronhit_wedn;
+name_orz + num_orz_week + name_bronhit + num_bronhit_week;
